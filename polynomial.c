ab42b0ef78ebd51b302b328b5ca313b51aaea9be
Bunny-work-hub <bandivishal2005@gmail.com>
2024-10-25 15:44:51 +0530

    Update polynomial.c

--- polynomial.c
+++ polynomial.c
@@ -1,67 +1,78 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<stdbool.h>
-struct node{
-int info;
-struct node *link;
+struct node {
+int cof_x;
+int pow_x;
+struct node * link;
 };
 typedef struct node * N;
-void insert_ra(int ,N );
-int insert_mid(N );
 void dis(N );
-int main(){
-    int a,iteam;
-    char a_1;
-    printf("enter the first elements in a linked list : ");
-    scanf("%d",&a);
-    N k,first=NULL;
-    k=(N)malloc(sizeof(struct node));
-     k->info=a;
-     k->link=NULL;
-     first=k;
-     while(true){
-          printf("would you like to insert an element : ");
-        scanf(" %c",&a_1);
-            if(a_1=='y'){
-            printf("enter the element : ");
-            scanf("%d",&iteam);
-            insert_ra(iteam,first);
+void insert_ra(int ,int , N);
+N insert_fr(int ,int);
+int main()
+{
+    int p;
+    char a;
+    N first=NULL;
+    while(true){
+       int a_1,b;
+       printf("would you like to add another polynomial : y/n \n ");
+       scanf(" %c",&a);
+       if(a=='y'){
+            while(true){
+                    printf("p=1 : go to next power p=2 : stop");
+            scanf("%d",&p);
+            if (p==1){
+       if (first==NULL){
+         printf("enter the power of x : ");
+       scanf("%d",&b);
+       printf("enter the cofficient of above power : ");
+       scanf("%d",&a_1);
+       first=insert_fr(a_1,b);
+       }
+       else{
+       printf("enter the power of x : ");
+       scanf("%d",&b);
+       printf("enter the cofficient of above power : ");
+       scanf("%d",&a_1);
+       insert_ra(a_1,b,first);
+       }
             }
             else{
                 break;
             }
-     }
-     insert_mid(first);
-     dis(first);
-}
-void insert_ra(int k,N first){
-N j,l=first;
-j=(N)malloc(sizeof(struct node));
-j->info=k;
-j->link=NULL;
-while(l->link!=NULL){
-    l=l->link;
-}
-l->link=j;
-}
-void dis(N l){
-while(l!=NULL){
-    printf("%d \n ",l->info);
-    l=l->link;
-}
-}
-int insert_mid(N first){
-    N k,l=first,p=NULL;
-    int k_1;
-    printf("enter the element : ");
-    scanf(" %d",&k_1);
-    k=(N)malloc(sizeof(struct node));
-    k->info=k_1;
-    k->link=NULL;
-    while(l->info < k->info){
-    p=l;
-    l=l->link;
+            }
     }
-     k->link=l;
-    p->link=k;
+    else{
+        break;
+    }
+    }
+    dis(first);
+}
+void insert_ra(int k,int k_1,N first){
+ N t,l=first;
+ t=(N)malloc(sizeof(struct node));
+ t->cof_x=k;
+ t->pow_x=k_1;
+ t->link=NULL;
+ while(l->link!=NULL){
+    l=l->link;
+ }
+ l->link=t;
+}
+N insert_fr(int k,int k_1){
+N t;
+t=(N)malloc(sizeof(struct node));
+t->cof_x=k;
+t->pow_x=k_1;
+t->link=NULL;
+return t;
+}
+void dis (N first){
+N t=first;
+while(t!=NULL){
+    printf("%d",t->cof_x);
+    t=t->link;
+}
 }
