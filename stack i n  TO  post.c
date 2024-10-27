389d0d0b2fe911170c9986fd921d4910ca8e5ceb
Bunny-work-hub <bandivishal2005@gmail.com>
2024-10-27 19:32:55 +0530

    Update stack i n  TO  post.c

--- stack i n  TO  post.c
+++ stack i n  TO  post.c
@@ -132,3 +132,138 @@ int pop_eval() {
     }
     return stack[top--];
 }
+
+
+                                                THE ABOVE CODE IS FOR GLOBAL DECLARATION 
+
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+#include<math.h>
+#include<stdlib.h>
+struct stack{
+    int top;
+    int capacity;
+    char *array;
+};
+typedef struct stack * N;
+int prece(char );
+N create_stack(char i[])
+{
+    N stack;
+    stack=(N)malloc(sizeof(struct stack));
+    stack->top=-1;
+    stack->capacity=strlen(i);
+    stack->array=(char * )malloc(stack->capacity * sizeof(char));
+    return stack;
+}
+char pop(N stack)
+{
+    if (stack->top==-1)
+        return -1;
+    return(stack->array[stack->top--]);
+}
+void push(N stack,char iteam){
+    if(stack->top == stack->capacity-1)
+        printf("stack is full");
+    stack->top=stack->top+1;
+    stack->array[stack->top]=iteam;
+}
+char peek(N stack){
+    return(stack->array[stack->top]);
+}
+int intopost(char i[],char p[],N stack){
+    int j=0;
+    char h;
+    for (int k=0;k<strlen(i);k++){
+        char symbol=i[k];
+        switch(symbol){
+        case '(':
+            push(stack,symbol);
+            break;
+        case')':
+            while(peek(stack)!='('){
+                p[j++]=pop(stack);
+            }
+            pop(stack);
+            break;
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+        case '^':
+            h=peek(stack);
+            while(prece(symbol) <= prece(h)){
+                    p[j++]=pop(stack);
+            }
+            push(stack,symbol);
+            break;
+        default :
+            p[j++]=symbol;
+            break;
+        }
+    }
+    while(stack->top !=-1){
+        p[j++]=pop(stack);
+    }
+    p[j]='\0';
+}
+int prece(char k){
+    switch(k){
+    case '+':
+    case '-':
+        return 1;
+    case '*':
+    case '/':
+        return 2;
+    case '^':
+        return 3;
+    default:
+        return 0;
+    }
+}
+int postfix(char p[],N stack){
+    int k=0;
+    int a,b;
+    for (k=0;k<strlen(p);k++){
+            char symbol=p[k];
+        if (p[k]>='0' && p[k]<='9'){
+            push(stack,p[k]-'0');
+        }
+        else{
+                a = pop(stack);
+            b = pop(stack);
+            switch (p[k]) {
+            case '+':
+                push(stack,b + a);
+                break;
+            case '-':
+                push(stack,b - a);
+                break;
+            case '*':
+                push(stack,b * a);
+                break;
+            case '/':
+                push(stack,b / a);
+                break;
+            case '^':
+                push(stack,pow(b, a));
+                break;
+            }
+        }
+
+    }
+    return pop(stack);
+}
+int main()
+{
+    char i[100],p[100];
+    N stack;
+    printf("enter the infix expression ");
+    gets(i);
+    stack=create_stack(i);
+    intopost(i,p,stack);
+    printf("%s",p);
+    int res=postfix(p,stack);
+    printf("\ result %d",res);
+}
